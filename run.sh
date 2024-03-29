d43f6bf58b262bb0878b3b16dab174e8cac37c03
Rémi Montagut <montagutremi64@gmail.com>
2024-03-29 14:33:31 +0100

    new

--- run.sh
+++ run.sh
@@ -1,40 +1,44 @@
 #!/bin/bash
+
+# Check if both hostname and IP address are provided
+if [ "$#" -ne 2 ]; then
+    echo "Usage: $0 <hostname> <ip_address>"
+    exit 1
+fi
+
+hostname="$1"
 ip_addr="$2"
 
+# Update and upgrade the system packages
 apt update && apt upgrade -y
 
-hostname "$1"
-chmod 777 /etc/hostname /etc/hosts
-sed "s/template-ubuntu/$1/g" /etc/hosts > /home/user/hosts.out
-rm /etc/hosts
-mv /home/user/hosts.out /etc/hosts
-echo "$1" > /etc/hostname
-chmod 644 /etc/hostname /etc/hosts
-service hostname down
-service hostname start
+# Set hostname and update related configuration files
+echo "$hostname" > /etc/hostname
+sed -i "s/template-ubuntu/$hostname/g" /etc/hosts
+
+# Restart hostname service
+systemctl restart systemd-hostnamed
+
+# Renew DHCP lease
 dhclient
 
-
-git --config
-git --config
-
+# Clone the repository
 git clone https://github.com/GL0WIK/New-linux-vm.git
-
 cd New-linux-vm
 
-three_numbers=$(echo $ip_addr | cut -d '.' -f 1-3)
+# Configure network settings
+gateway=$(echo "$ip_addr" | cut -d '.' -f 1-3)".1"
+echo "Gateway: $gateway"
+sed -i "s/ip-addr/$ip_addr/g" 00-installer-config.yaml
+sed -i "s/gateway/$gateway/g" 00-installer-config.yaml
 
-gateway="$three_numbers.1"
+# Copy modified configuration file to netplans directory
+cp 00-installer-config.yaml /etc/netplan/00-installer-config.yaml
 
-echo "Adresse IP modifiée : $gateway"
+# Apply network configuration
+netplan apply
 
-while IFS= read -r line; do
-    modified_line="${line//ip-addr/$ip_addr}"
-    modified_line="${modified_line//gateway/$gateway}"
-    echo "$modified_line"
-done < "00-installer-config.yaml" > temp.yaml
+echo "Network configuration applied."
 
-# Remplacer le fichier original par le fichier temporaire
-mv temp.yaml /etc/netplans/00-installer-config.yaml
-
-echo "Le fichier ici.yaml a été mis à jour avec les modifications."
+# Inform user about completion
+echo "Setup completed successfully."
